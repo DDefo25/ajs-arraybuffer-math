397437c112b16cb712214c2d2cecb87aa480d294
ddefo25 <ispumamm@gmail.com>
2022-10-28 13:13:06 +0200

    fix Character constructor

--- src/js/__tests__/Bowman.test.js
+++ src/js/__tests__/Bowman.test.js
@@ -4,6 +4,15 @@ test("creating new Bowman", () => {
     const expectings = {
         name: "Ivan", health: 100, level: 1, type: "Bowman", attack: 25, defence: 25,
     };
-    const result = new Bowman({ name: "Ivan" });
+    const result = new Bowman("Ivan");
     expect(result).toEqual(expectings);
+});
+
+test("error of wrong type", () => {
+    const expecting = new Error("Тип персонажа не соответствует существующим");
+    function createBowman() {
+        const result = new Bowman("Ivan", "Tank");
+        return result;
+    }
+    expect(createBowman).toThrow(expecting);
 });
